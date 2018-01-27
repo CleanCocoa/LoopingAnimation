d58947736f0acc7edd059b253bf22bd6da462485
Christian Tietze <christian.tietze@gmail.com>
2018-01-27 10:18:47 +0100

    Initial Commit

---
+++ LoopingAnimation/LoopingAnimation.h
@@ -0,0 +1,19 @@
+//
+//  LoopingAnimation.h
+//  LoopingAnimation
+//
+//  Created by Christian Tietze on 27.01.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LoopingAnimation.
+FOUNDATION_EXPORT double LoopingAnimationVersionNumber;
+
+//! Project version string for LoopingAnimation.
+FOUNDATION_EXPORT const unsigned char LoopingAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoopingAnimation/PublicHeader.h>
+
+
